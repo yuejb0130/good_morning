63a85085ad77831a3b4db19dbb43310b35470783
NN9120 <1910063376@qq.com>
2022-08-24 20:45:04 +0800

    增加注释及说明

--- main.py
+++ main.py
@@ -20,7 +20,34 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
+# 测试数据
+# start_date = "2022-02-04"
+# city = "西安"
+# birthday = "09-04"
+# app_id = "wx49e7eddadffbf05e"
+# app_secret = "d25ab0ba046f79c110b620a9837da35d"
+# user_id = "o_Tnm6BNGYVt4b7Rd6rn91ucKIbk,o_Tnm6JVuaTN4U2URm4oSbWKLZ5U"
+# template_id = "OdhnBv_P8t7p-3GnFblHgdURfAkXw3IsPr8aqrhqhWg"
 
+# 定时发送数据
+# o_Tnm6JVuaTN4U2URm4oSbWKLZ5U,o_Tnm6GhHixABnUhzXOqhympo7ZI
+
+""""
+宁仙子的专属消息提醒
+
+{{day.DATA}}  {{week.DATA}}
+城市：{{city.DATA}}
+天气：{{weather.DATA}}
+最低温度：{{low.DATA}} ℃
+当前温度：{{temperature.DATA}} ℃
+最高温度：{{high.DATA}} ℃
+今天是我们在一起：{{love_days.DATA}} 天
+距离宁宁的生日还有：{{birthday_left.DATA}}天
+{{words.DATA}}
+"""
+
+
+# 获取天气以及温度
 def get_weather():
     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
     res = requests.get(url).json()
@@ -28,11 +55,13 @@ def get_weather():
     return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high'])
 
 
+# 计算纪念日天数
 def get_count():
     delta = today - datetime.strptime(start_date, "%Y-%m-%d")
     return delta.days
 
 
+# 计算距离生日天数
 def get_birthday():
     next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
     if next < datetime.now():
@@ -40,6 +69,7 @@ def get_birthday():
     return (next - today).days
 
 
+# 获取文案
 def get_words():
     words = requests.get("https://api.shadiao.pro/chp")
     if words.status_code != 200:
@@ -47,6 +77,7 @@ def get_words():
     return words.json()['data']['text']
 
 
+# 获取字体颜色
 def get_random_color():
     return "#%06x" % random.randint(0, 0xFFFFFF)
 
